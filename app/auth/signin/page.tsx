63222e219752eb5bdd3cad8f18a2652bbb720dd0
kalyanasundar8 <sundar31100@gmail.com>
2025-11-12 22:02:10 +0530

    Signin page added, validation added for both signup and signin

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -6,29 +6,20 @@ import { useForm } from "react-hook-form";
 import z from "zod";
 
 // ZOD form validation schema
-const SignUpFormSchema = z
-  .object({
-    username: z.string().min(3, "Username must be atleast 3 characters long"),
-    email: z.email("Invalid email address"),
-    password: z
-      .string()
-      .min(6, "Password must be atleast 6 characters long")
-      .regex(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-        "Password must contains atleast one letter and one number"
-      ),
-    confirmpassword: z.string().min(1, "Please confirm your password"),
-  })
-  .refine((data) => data.password === data.confirmpassword, {
-    message: "Password do not match",
-    path: ["confirmpassword"],
-  });
+const SignInFormSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be atleast 6 characters long")
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      "Password must contains atleast one letter and one number"
+    ),
+});
 
 interface FormData {
-  username: string;
   email: string;
   password: string;
-  confirmpassword: string;
 }
 
 const page = () => {
@@ -38,12 +29,10 @@ const page = () => {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<FormData>({
-    resolver: zodResolver(SignUpFormSchema),
+    resolver: zodResolver(SignInFormSchema),
     defaultValues: {
-      username: "",
       email: "",
       password: "",
-      confirmpassword: "",
     },
   });
 
@@ -72,26 +61,6 @@ const page = () => {
             onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col space-y-3 w-full sm:w-3/4 md:w-2/6 lg:w-1/3 transition-all duration-300 transform"
           >
-            <div className="flex flex-col items-start">
-              <label htmlFor="username" className="text-sm">
-                Username*
-              </label>
-              <input
-                {...register("username", { required: true })}
-                type="text"
-                placeholder="username"
-                className="w-full px-2 py-1 md:p-2 border-2 border-gray-300/60 rounded-md focus:outline-blue-500 placeholder:text-sm"
-              />
-              <p
-                className={`text-sm p-1 text-red-600 transition-all duration-300 transform ${
-                  errors.username
-                    ? "opacity-100 translate-y-0"
-                    : "opacity-0 -translate-y-1"
-                }`}
-              >
-                {errors.username?.message || " "}
-              </p>
-            </div>
             <div className="flex flex-col items-start">
               <label htmlFor="email" className="text-sm">
                 Email*
@@ -132,26 +101,6 @@ const page = () => {
                 {errors.password?.message}
               </p>
             </div>
-            <div className="flex flex-col items-start">
-              <label htmlFor="confirmpassword" className="text-sm">
-                Confirm Password*
-              </label>
-              <input
-                {...register("confirmpassword", { required: true })}
-                type="password"
-                placeholder="confirm password"
-                className="w-full px-2 py-1 md:p-2 border-2 border-gray-300/60 rounded-md focus:outline-blue-500 placeholder:text-sm"
-              />
-              <p
-                className={`text-sm p-1 text-red-600 transition-all duration-300 transform ${
-                  errors.confirmpassword
-                    ? "opacity-100 translate-y-0"
-                    : "opacity-0 -translate-y-1"
-                }`}
-              >
-                {errors.confirmpassword?.message}
-              </p>
-            </div>
             <button
               disabled={!isValid}
               className={`group flex items-center text-center justify-center gap-2 w-full p-3 bg-black text-white rounded-sm ${
@@ -165,12 +114,12 @@ const page = () => {
               />
             </button>
             <div className="text-sm flex items-center justify-center space-x-2 mt-4">
-              <p className="text-gray-400">Already have an account?</p>
+              <p className="text-gray-400">I dont have an account?</p>
               <Link
-                href="/auth/signin"
+                href="/auth/signup"
                 className="font-semibold tracking-tighter opacity-50 hover:opacity-100 transition-all  duration-300"
               >
-                Sign In
+                Sign Up
               </Link>
             </div>
           </form>
